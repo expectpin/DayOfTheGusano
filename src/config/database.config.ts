dda0a0222ba21750a52ef8e706397fed9092118d
Evandro Picanco <expectpin@users.noreply.github.com>
2024-05-23 19:51:16 -0300

    Update database connection parameters

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,8 +1,8 @@
 export default {
   typeorm: {
-    host: process.env.DB_HOST || 'host.docker.internal',
+    host: process.env.DB_HOST || 'postgres',
     port: process.env.DB_PORT || 5432,
-    username: process.env.DB_USERNAME || 'root',
+    username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'usermanagement',
   },
